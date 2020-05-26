6d9aeb633de985357e40d609871f818b44546752
You seungheon <heonwogur@gmail.com>
2020-05-26 22:11:29 +0900

    윤년을 구하는 함수 is leap 함수 구현

--- calendar.py
+++ calendar.py
@@ -1,26 +1,31 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
 def main():
     year = int(input("년도를 입력하세요: "))
     month = int(input("월을 입력하세요: "))
     day = int(input("일을 입력하세요: "))
 
     date = {'year': year, 'month': month, 'day': day}
-
+    month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day_names = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"]
     total_days = 0
 
     # 직전년도까지의 총 일 수 계산
     for i in range(1, date["year"]):
-        if i % 4 == 0:
-            if i % 100 == 0:
-                if i % 400 == 0:
-                    total_days = total_days + 366
-                else:
-                    total_days = total_days + 365
-            else:
-                total_days = total_days + 366
+        if is_leap(i):
+            total_days = total_days + 366
         else:
             total_days = total_days + 365
-
-    month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     for i in range(1, date["month"]):
         total_days = total_days + month_days[i]
@@ -40,8 +45,6 @@ def main():
 
     total_days = total_days + day
 
-    day_names = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"]
-    # print(total_days)
     print(
         f"{date['year']}년 {date['month']}월 {date['day']}일은 {day_names[total_days % 7]} 입니다.")
 
