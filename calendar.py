0839cb249cc5981eaa12fd3d274b6652ec939cfc
You seungheon <heonwogur@gmail.com>
2020-05-26 21:24:37 +0900

    요일 계산기 완성 - 총날수 계산 - 윤년 계산

--- calendar.py
+++ calendar.py
@@ -6,6 +6,7 @@ date = {'year': year, 'month': month, 'day': day}
 
 total_days = 0
 
+# 직전년도까지의 총 일 수 계산
 for i in range(1, date["year"]):
     if i % 4 == 0: 
         if i % 100 == 0:
@@ -29,14 +30,15 @@ if date["year"] % 4 == 0:
             if date["month"] >= 3:
                 total_days = total_days + 1
         else:
-            total_days = total_days + 0
+            pass
     else: 
         if date["month"] >= 3:
             total_days = total_days + 1
 else:
-    total_days = total_days + 0
+    pass
 
 total_days = total_days + day
 
+day_names = ["일요일","월요일","화요일","수요일","목요일","금요일","토요일"]
 #print(total_days)
-print(total_days%7)
+print(f"{date['year']}년 {date['month']}월 {date['day']}일은 {day_names[total_days % 7]} 입니다.")
