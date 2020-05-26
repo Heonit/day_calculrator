535ba7e2bab4f808b5bd85db3a5c9ade4a849c1d
You seungheon <heonwogur@gmail.com>
2020-05-26 21:59:39 +0900

    요일 계산기 - main 함수로 구조화

--- calendar.py
+++ calendar.py
@@ -1,44 +1,50 @@
-year = int(input("년도를 입력하세요: "))
-month = int(input("월을 입력하세요: "))
-day = int(input("일을 입력하세요: "))
+def main():
+    year = int(input("년도를 입력하세요: "))
+    month = int(input("월을 입력하세요: "))
+    day = int(input("일을 입력하세요: "))
 
-date = {'year': year, 'month': month, 'day': day}
+    date = {'year': year, 'month': month, 'day': day}
 
-total_days = 0
+    total_days = 0
 
-# 직전년도까지의 총 일 수 계산
-for i in range(1, date["year"]):
-    if i % 4 == 0: 
-        if i % 100 == 0:
-            if i % 400 == 0:
-                total_days = total_days + 366
+    # 직전년도까지의 총 일 수 계산
+    for i in range(1, date["year"]):
+        if i % 4 == 0:
+            if i % 100 == 0:
+                if i % 400 == 0:
+                    total_days = total_days + 366
+                else:
+                    total_days = total_days + 365
             else:
-                total_days = total_days + 365
-        else: 
-            total_days = total_days + 366
-    else:
-        total_days = total_days + 365
+                total_days = total_days + 366
+        else:
+            total_days = total_days + 365
 
-month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-for i in range(1, date["month"]):
-    total_days = total_days + month_days[i]
+    for i in range(1, date["month"]):
+        total_days = total_days + month_days[i]
 
-if date["year"] % 4 == 0: 
-    if date["year"] % 100 == 0:
-        if date["year"] % 400 == 0:
+    if date["year"] % 4 == 0:
+        if date["year"] % 100 == 0:
+            if date["year"] % 400 == 0:
+                if date["month"] >= 3:
+                    total_days = total_days + 1
+            else:
+                pass
+        else:
             if date["month"] >= 3:
                 total_days = total_days + 1
-        else:
-            pass
-    else: 
-        if date["month"] >= 3:
-            total_days = total_days + 1
-else:
-    pass
+    else:
+        pass
 
-total_days = total_days + day
+    total_days = total_days + day
 
-day_names = ["일요일","월요일","화요일","수요일","목요일","금요일","토요일"]
-#print(total_days)
-print(f"{date['year']}년 {date['month']}월 {date['day']}일은 {day_names[total_days % 7]} 입니다.")
+    day_names = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"]
+    # print(total_days)
+    print(
+        f"{date['year']}년 {date['month']}월 {date['day']}일은 {day_names[total_days % 7]} 입니다.")
+
+
+if __name__ == "__main__":
+    main()
